90904563e577242a0bf77aa6277a52720dda4c33
zbs <zbs1016@163.com>
2018-12-26 20:23:16 +0800

    add

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"github.com/gin-gonic/gin"
+	_ "zbs/v1-blogger/dao"
+)
 
+func main() {
+	r := gin.Default()
+
+	// 设置以 debug 模式运行gin
+	// 生产中使用 gin.ReleaseMode 模式
+	gin.SetMode(gin.DebugMode)
+
+	r.Run(":9000")
 }
